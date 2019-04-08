de5fd0a79e36648d912313fe93b192ee95047af3
yuchlin <yuchlin@ebay.com>
2019-04-07 23:42:16 -0700

    Fix for Nested Objects and Arrays

--- src/main/java/com/owen/jackson/json/demo/Driver.java
+++ src/main/java/com/owen/jackson/json/demo/Driver.java
@@ -18,7 +18,7 @@ public class Driver {
 
             // read JSON file and map/convert tp JAVA POJO
             // data/sample-lite.json
-            Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+            Student student = mapper.readValue(new File("data/sample-full.json"), Student.class);
 
             // print first name and last name
             System.out.println("First name = " + student.getFirstName());
--- src/main/java/com/owen/jackson/json/demo/Student.java
+++ src/main/java/com/owen/jackson/json/demo/Student.java
@@ -10,6 +10,10 @@ public class Student {
     private String lastName;
     private boolean active;
 
+    private Address address;
+
+    private String[] languages;
+
     public Student() {
     }
 
@@ -43,5 +47,21 @@ public class Student {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
     }
 }
